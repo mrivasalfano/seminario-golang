b8dca77ed0efbd8dde6245a88bf18c6d920a3603
mrivasalfano <mrivasalfano@gmail.com>
2020-11-05 17:41:08 -0300

    crud con var global de recetas

--- actividad-1/recetas.go
+++ actividad-1/recetas.go
@@ -1,50 +1,108 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	// "errors"
 )
 
-type paginaRecetas struct {
-	nombre  string
-	recetas []receta
-}
+var listaRecetas []receta
 
 type receta struct {
+	id         int
 	nombre     string
 	duracion   int
 	dificultad string
 }
 
-func (p paginaRecetas) addReceta(nombre string, duracion int, dificultad string) paginaRecetas {
-	r := receta{nombre, duracion, dificultad}
-	p.recetas = append(p.recetas, r)
-	return p
+func addReceta(id int, nombre string, duracion int, dificultad string) {
+	r := receta{id, nombre, duracion, dificultad}
+	listaRecetas = append(listaRecetas, r)
 }
 
-func (p paginaRecetas) getRecetas() []receta {
-	return p.recetas
+func getRecetas() []receta {
+	return listaRecetas
+}
+
+func getReceta(id int) (receta, error) {
+	var receta receta
+
+	for _, rec := range listaRecetas {
+		if rec.id == id {
+			receta = rec
+			return receta, nil
+		}
+	}
+
+	return receta, errors.New("No se encontró receta con este id")
+}
+
+func updateReceta(id int, nombre string, duracion int, dificultad string) {
+	for i := 0; i < len(listaRecetas); i++ {
+		if listaRecetas[i].id == id {
+			listaRecetas[i].nombre = nombre
+			listaRecetas[i].duracion = duracion
+			listaRecetas[i].dificultad = dificultad
+		}
+	}
+}
+
+func deleteReceta(id int) {
+	for i, receta := range listaRecetas {
+		if receta.id == id {
+			listaRecetas = listaRecetas[:i+copy(listaRecetas[i:], listaRecetas[i+1:])]
+		}
+	}
 }
 
 func main() {
-	//creo slice de recetas con capacidad inicial de 10
-	var sliceRecetas = make([]receta, 10)
+	//creo recetas
+	addReceta(1, "Tortilla de papa", 30, "Media")
+	addReceta(2, "Bifes a la criolla", 60, "Baja")
 
-	//creo pagina de recetas
-	paginaRecetas := paginaRecetas{"Recetas todos los días", sliceRecetas}
-
-	//agrego recetas a la pagina
-	//Aclaración: el método devuelve una nueva paginaRecetas ya que dentro lo modifico
-	//pero si no lo devuelvo se pierde el cambio ya que son estructuras distintas
-	//o sea están en distintos lugares de memoria
-	paginaRecetas = paginaRecetas.addReceta("Tortilla de papa", 30, "Media")
-	paginaRecetas = paginaRecetas.addReceta("Bifes a la criolla", 60, "Baja")
-
-	//pido las recetas a la pagina
-	recetas := paginaRecetas.getRecetas()
+	//pido las recetas
+	recetas := getRecetas()
 
 	//recorro e imprimo las recetas
 	for _, v := range recetas {
 		fmt.Println(v)
 	}
+
+	//pido receta por id válido
+	receta1, error := getReceta(1)
+
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(receta1)
+	}
+
+	//pido receta por id inválido
+	receta2, error := getReceta(3)
+
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(receta2)
+	}
+
+	//edito receta
+	updateReceta(1, "Editada", 20, "Alta")
+	receta3, error := getReceta(1)
+
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(receta3)
+	}
+
+	//borro receta
+	deleteReceta(1)
+	receta4, error := getReceta(1)
+
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(receta4)
+	}
+
 }
