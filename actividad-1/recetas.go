80dd0885b1c1e74475a440df0f74507b0d2cda93
mrivasalfano <mrivasalfano@gmail.com>
2020-11-05 17:55:18 -0300

    agrego crud

--- actividad-1/recetas.go
+++ actividad-1/recetas.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	// "errors"
 )
@@ -11,18 +12,50 @@ type paginaRecetas struct {
 }
 
 type receta struct {
+	id         int
 	nombre     string
 	duracion   int
 	dificultad string
 }
 
-func (p *paginaRecetas) addReceta(nombre string, duracion int, dificultad string) {
-	r := receta{nombre, duracion, dificultad}
+func (p *paginaRecetas) addReceta(id int, nombre string, duracion int, dificultad string) {
+	r := receta{id, nombre, duracion, dificultad}
 	p.recetas = append(p.recetas, r)
 }
 
 func (p *paginaRecetas) getRecetas() []receta {
 	return p.recetas
+}
+
+func (p *paginaRecetas) getReceta(id int) (receta, error) {
+	var receta receta
+
+	for _, rec := range p.recetas {
+		if rec.id == id {
+			receta = rec
+			return receta, nil
+		}
+	}
+
+	return receta, errors.New("No se encontró receta con este id")
+}
+
+func (p *paginaRecetas) updateReceta(id int, nombre string, duracion int, dificultad string) {
+	for i := 0; i < len(p.recetas); i++ {
+		if p.recetas[i].id == id {
+			p.recetas[i].nombre = nombre
+			p.recetas[i].duracion = duracion
+			p.recetas[i].dificultad = dificultad
+		}
+	}
+}
+
+func (p *paginaRecetas) deleteReceta(id int) {
+	for i, receta := range p.recetas {
+		if receta.id == id {
+			p.recetas = p.recetas[:i+copy(p.recetas[i:], p.recetas[i+1:])]
+		}
+	}
 }
 
 func main() {
@@ -33,8 +66,8 @@ func main() {
 	paginaRecetas := paginaRecetas{"Recetas todos los días", sliceRecetas}
 
 	//agrego recetas a la pagina
-	paginaRecetas.addReceta("Tortilla de papa", 30, "Media")
-	paginaRecetas.addReceta("Bifes a la criolla", 60, "Baja")
+	paginaRecetas.addReceta(1, "Tortilla de papa", 30, "Media")
+	paginaRecetas.addReceta(2, "Bifes a la criolla", 60, "Baja")
 
 	//pido las recetas a la pagina
 	recetas := paginaRecetas.getRecetas()
